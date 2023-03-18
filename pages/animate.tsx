ab0f887ae81c29ef360424068d4eed369f2ab325
HASSAN <hassan.aburesha@gmail.com>
2023-03-18 17:34:04 +0100

    added animate page for animations when in view ;-)

---
+++ pages/animate.tsx
@@ -0,0 +1,37 @@
+import { useRef, ReactNode } from "react";
+import { useInView } from "framer-motion";
+import styles from "./animate.module.css";
+
+function Section({ children }: { children: ReactNode }) {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+
+    return (
+        <div className={styles["section-container"]}>
+            <section ref={ref}>
+                <span
+                    className={styles.section}
+                    style={{
+                        transform: isInView ? "none" : "translateX(-200px)",
+                        opacity: isInView ? 1 : 0,
+                        transition:
+                            "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+                    }}
+                >
+                    {children}
+                </span>
+            </section>
+        </div>
+    );
+}
+
+export default function Animate() {
+    return (
+        <>
+            <Section>Animate</Section>
+            <Section>when</Section>
+            <Section>in</Section>
+            <Section>view!</Section>
+        </>
+    );
+}
