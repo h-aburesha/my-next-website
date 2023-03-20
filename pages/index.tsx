b7da2724d999d2119b9e15bb8555d0ae29bee0f8
HASSAN <hassan.aburesha@gmail.com>
2023-03-20 14:09:06 +0100

    working particles animation yuhuu

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import ParticlesContainer from "./particles";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
         <>
+            <ParticlesContainer />
             <div className="landing-container">
                 <div className="navigation-bar">
                     <a href="/projects">
