327d1fba7679c8e6aa7fc9cb0bba39adda9a895b
HASSAN <hassan.aburesha@gmail.com>
2023-03-22 20:24:10 +0100

    added download file on click

--- pages/about.tsx
+++ pages/about.tsx
@@ -21,7 +21,7 @@ const AboutMe = () => {
                 <a target={"_blank"} href="https://github.com/h-aburesha">
                     <FaGithub />
                 </a>
-                <a href="/your-resume.pdf" download>
+                <a href="/hassan_aburesha_cv.pdf" download>
                     <FaDownload />
                 </a>
             </div>
