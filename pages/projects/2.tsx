91b60b583ba59edf2e73248dae400713a156ff48
HASSAN <hassan.aburesha@gmail.com>
2023-04-02 22:34:45 +0200

    project2 is done

--- pages/projects/2.tsx
+++ pages/projects/2.tsx
@@ -2,7 +2,10 @@ import { motion, useScroll, useSpring } from "framer-motion";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import Image from "next/image";
-import jamLogo from "../../public/jamlogo.png";
+import { Carousel } from "@mantine/carousel";
+import jam1 from "../../public/jam_pics/jam1.png";
+import jam2 from "../../public/jam_pics/jam2.png";
+import jam3 from "../../public/jam_pics/jam3.png";
 
 function Project2() {
     const { scrollYProgress } = useScroll();
@@ -58,77 +61,109 @@ function Project2() {
                 <h2 style={{ fontWeight: "bolder" }}>Description</h2>
                 <hr />
                 <p>
-                    A React Native fitness tracking mobile application, designed
-                    based on my{" "}
-                    <Link href="/projects/5">previous UX/UI Study</Link>.
+                    A social network platform using React/Redux, Express-Server
+                    & PostgresSQL.
                 </p>
                 <h2 style={{ fontWeight: "bolder" }}>Features</h2>
                 <hr />
                 <ul>
                     <li>
-                        Barcode scanning using the phone&apos;s camera to show
-                        nutritional information of products.
+                        Authentication: User registration, login, and password
+                        reset functionality are available to ensure secure
+                        access to the app.
                     </li>
-                    <li>Tracking of body metrics.</li>
-                    <li>Capability for personal notes and reminders.</li>
-                    <li>Recipe suggestions.</li>
-                    <li>Metrics page displaying total macronutrient intake.</li>
                     <li>
-                        Personal profile page with BMI, photo, and dark/light
-                        mode toggle
+                        User Profile: Users can customize their profile with bio
+                        and profile picture updates to reflect their personal
+                        preferences and interests.
+                    </li>
+                    <li>
+                        Friend Search: Users can search for other users and send
+                        friend requests to connect with people they know or
+                        share similar interests.
+                    </li>
+                    <li>
+                        Friend Requests: Users can manage their friend requests
+                        by accepting or rejecting incoming requests, as well as
+                        sending friend requests to others.
+                    </li>
+                    <li>
+                        Friend List: Users can view their current friends, as
+                        well as any outstanding friend requests they have sent
+                        or received. This helps users keep track of their social
+                        connections and expand their network as desired.
                     </li>
                 </ul>
-                <h2 style={{ fontWeight: "bolder" }}>
-                    Challenges & Motivation
-                </h2>
+                <h2 style={{ fontWeight: "bolder" }}>Challenges</h2>
                 <hr />
                 <p>
-                    When I first set out to develop a fitness tracking mobile
-                    app based on my previous UX/UI study, my motivation was
-                    driven by the desire to bring my own designs to life.
-                    However, I quickly realized that transferring designs from
-                    Figma to React Native was not a straightforward process.
-                    Despite this challenge, I remained undeterred and determined
-                    to find a way to make it work.
+                    Learning React: As a first-time React user, I faced a steep
+                    learning curve and had to adapt to a completely different
+                    style of programming compared to my previous experience with
+                    Vue. At first, I found the use of class components quite
+                    cumbersome, with numerous instances of &apos;.this&apos;
+                    making the code less readable. Eventually, I was able to
+                    refactor all components to function components, which
+                    greatly improved readability and practicality.
                 </p>
                 <p>
-                    Although mobile development was not a part of my formal
-                    studies, I had become proficient in JavaScript and React
-                    after completing my Social Network project. My excitement
-                    was palpable when I discovered React Native, which allowed
-                    me to write mobile applications in JavaScript/TS that could
-                    be readily available across different platforms in their
-                    native languages. This meant that the application could be
-                    written once and still be usable on both iOS and Android.
+                    Props Drilling: One of the challenges I faced in building
+                    the app was passing functions and properties deep within the
+                    component hierarchy, which can make it difficult to keep
+                    track of everything. Initially, I attempted to avoid this by
+                    fetching all user data from the database and passing it from
+                    the top-level &apos;App&apos; component. However, I soon
+                    realized that this approach wasn&apos;t scalable or
+                    practical. Fortunately, I was able to leverage Redux to
+                    manage global state and avoid excessive props drilling.
                 </p>
                 <p>
-                    Setting up a development environment for a newcomer can be
-                    daunting, especially if they are not familiar with native
-                    CLI. For this reason, I would strongly recommend using an
-                    Expo CLI, which can get you started in just a few minutes.
-                    However, I found that ejecting the application from Expo
-                    could be difficult. As a result, after a lot of research and
-                    setup, I am currently refactoring the project again using a
-                    Native CLI. Despite the initial setbacks, I am determined to
-                    see this project through to the end and make it available to
-                    anyone looking to track their fitness goals.
-                </p>
-                <p>
-                    In conclusion, while the journey to develop a React Native
-                    mobile app for fitness tracking has not been without its
-                    challenges, it has been a rewarding experience. From
-                    grappling with design transfers to setting up a development
-                    environment, I have been able to surmount each challenge and
-                    learned valuable skills along the way. Despite the setbacks,
-                    my motivation remains undimmed, and I look forward to
-                    launching this app to help individuals track their fitness
-                    goals in a convenient and user-friendly way.
+                    Redux: While Redux provided an effective solution to the
+                    problem of props drilling, it was also the most
+                    time-consuming and challenging aspect of the project.
+                    Setting up Redux and getting the hang of its global state
+                    management system took a lot of effort, but the payoff was
+                    worth it. With Redux, I was able to implement a single
+                    source of truth for my app&apos;s state, which greatly
+                    simplified data management and made my code more
+                    maintainable. Moreover, Redux-Toolkit provided some useful
+                    browser DevTools and graphical representations of state that
+                    were quite helpful.
                 </p>
                 <h2 style={{ fontWeight: "bolder" }}>Gallery</h2>
                 <hr />
                 <br />
-                Carousel
-                <br />
+
+                <Carousel
+                    maw="auto"
+                    mx="auto"
+                    withIndicators
+                    height="auto"
+                    controlsOffset="xs"
+                    loop
+                >
+                    <Carousel.Slide>
+                        <Image
+                            src={jam1}
+                            alt="Bold fitness tracking app home page built by react native over expo client"
+                            placeholder="blur"
+                        />
+                    </Carousel.Slide>
+                    <Carousel.Slide>
+                        <Image
+                            src={jam2}
+                            alt="Bold fitness tracking app macronutients page"
+                            placeholder="blur"
+                        />
+                    </Carousel.Slide>
+                    <Carousel.Slide>
+                        <Image
+                            src={jam3}
+                            alt="Bold fitness tracking app native recepies carousel"
+                            placeholder="blur"
+                        />
+                    </Carousel.Slide>
+                </Carousel>
             </article>
             <br />
 
