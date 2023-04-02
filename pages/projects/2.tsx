ddaf27c19412b698c96cccf694ad233d1889c2f2
HASSAN <hassan.aburesha@gmail.com>
2023-04-02 18:47:24 +0200

    project2 skeletion

--- pages/projects/2.tsx
+++ pages/projects/2.tsx
@@ -1,5 +1,8 @@
 import { motion, useScroll, useSpring } from "framer-motion";
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+import Image from "next/image";
+import jamLogo from "../../public/jamlogo.png";
 
 function Project2() {
     const { scrollYProgress } = useScroll();
@@ -27,110 +30,105 @@ function Project2() {
             </nav>
 
             <h1>
-                <code>Jam</code> with spring smoothing
+                <code style={{ fontWeight: "bold", fontSize: "1.5em" }}>
+                    JAM{" "}
+                </code>
+                <a
+                    target={"_blank"}
+                    href="https://github.com/h-aburesha/react-native-app-bold"
+                >
+                    <FaGithub />
+                </a>
+                <br />
+                <code
+                    style={{
+                        fontStyle: "italic",
+                        fontWeight: "lighter",
+                        fontSize: "0.8em",
+                    }}
+                >
+                    React/Redux Social Network
+                </code>
             </h1>
 
             <article>
+                <h2 style={{ fontWeight: "bolder" }}>Description</h2>
+                <hr />
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Aliquam ac rhoncus quam.
+                    A React Native fitness tracking mobile application, designed
+                    based on my{" "}
+                    <Link href="/projects/5">previous UX/UI Study</Link>.
+                </p>
+                <h2 style={{ fontWeight: "bolder" }}>Features</h2>
+                <hr />
+                <ul>
+                    <li>
+                        Barcode scanning using the phone&apos;s camera to show
+                        nutritional information of products.
+                    </li>
+                    <li>Tracking of body metrics.</li>
+                    <li>Capability for personal notes and reminders.</li>
+                    <li>Recipe suggestions.</li>
+                    <li>Metrics page displaying total macronutrient intake.</li>
+                    <li>
+                        Personal profile page with BMI, photo, and dark/light
+                        mode toggle
+                    </li>
+                </ul>
+                <h2 style={{ fontWeight: "bolder" }}>
+                    Challenges & Motivation
+                </h2>
+                <hr />
+                <p>
+                    When I first set out to develop a fitness tracking mobile
+                    app based on my previous UX/UI study, my motivation was
+                    driven by the desire to bring my own designs to life.
+                    However, I quickly realized that transferring designs from
+                    Figma to React Native was not a straightforward process.
+                    Despite this challenge, I remained undeterred and determined
+                    to find a way to make it work.
                 </p>
                 <p>
-                    Fringilla quam urna. Cras turpis elit, euismod eget ligula
-                    quis, imperdiet sagittis justo. In viverra fermentum ex ac
-                    vestibulum. Aliquam eleifend nunc a luctus porta. Mauris
-                    laoreet augue ut felis blandit, at iaculis odio ultrices.
-                    Nulla facilisi. Vestibulum cursus ipsum tellus, eu tincidunt
-                    neque tincidunt a.
-                </p>
-                <h2>Sub-header</h2>
-                <p>
-                    In eget sodales arcu, consectetur efficitur metus. Duis
-                    efficitur tincidunt odio, sit amet laoreet massa fringilla
-                    eu.
+                    Although mobile development was not a part of my formal
+                    studies, I had become proficient in JavaScript and React
+                    after completing my Social Network project. My excitement
+                    was palpable when I discovered React Native, which allowed
+                    me to write mobile applications in JavaScript/TS that could
+                    be readily available across different platforms in their
+                    native languages. This meant that the application could be
+                    written once and still be usable on both iOS and Android.
                 </p>
                 <p>
-                    Pellentesque id lacus pulvinar elit pulvinar pretium ac non
-                    urna. Mauris id mauris vel arcu commodo venenatis. Aliquam
-                    eu risus arcu. Proin sit amet lacus mollis, semper massa ut,
-                    rutrum mi.
+                    Setting up a development environment for a newcomer can be
+                    daunting, especially if they are not familiar with native
+                    CLI. For this reason, I would strongly recommend using an
+                    Expo CLI, which can get you started in just a few minutes.
+                    However, I found that ejecting the application from Expo
+                    could be difficult. As a result, after a lot of research and
+                    setup, I am currently refactoring the project again using a
+                    Native CLI. Despite the initial setbacks, I am determined to
+                    see this project through to the end and make it available to
+                    anyone looking to track their fitness goals.
                 </p>
                 <p>
-                    Sed sem nisi, luctus consequat ligula in, congue sodales
-                    nisl.
+                    In conclusion, while the journey to develop a React Native
+                    mobile app for fitness tracking has not been without its
+                    challenges, it has been a rewarding experience. From
+                    grappling with design transfers to setting up a development
+                    environment, I have been able to surmount each challenge and
+                    learned valuable skills along the way. Despite the setbacks,
+                    my motivation remains undimmed, and I look forward to
+                    launching this app to help individuals track their fitness
+                    goals in a convenient and user-friendly way.
                 </p>
-                <p>
-                    Vestibulum bibendum at erat sit amet pulvinar. Pellentesque
-                    pharetra leo vitae tristique rutrum. Donec ut volutpat ante,
-                    ut suscipit leo.
-                </p>
-                <h2>Sub-header</h2>
-                <p>
-                    Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum
-                    fringilla aliquet. Pellentesque auctor vehicula malesuada.
-                    Aliquam id feugiat sem, sit amet tempor nulla. Quisque
-                    fermentum felis faucibus, vehicula metus ac, interdum nibh.
-                    Curabitur vitae convallis ligula. Integer ac enim vel felis
-                    pharetra laoreet. Interdum et malesuada fames ac ante ipsum
-                    primis in faucibus. Pellentesque hendrerit ac augue quis
-                    pretium.
-                </p>
-                <p>
-                    Morbi ut scelerisque nibh. Integer auctor, massa non dictum
-                    tristique, elit metus efficitur elit, ac pretium sapien nisl
-                    nec ante. In et ex ultricies, mollis mi in, euismod dolor.
-                </p>
-                <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
-                <p>
-                    Pellentesque id lacus pulvinar elit pulvinar pretium ac non
-                    urna. Mauris id mauris vel arcu commodo venenatis. Aliquam
-                    eu risus arcu. Proin sit amet lacus mollis, semper massa ut,
-                    rutrum mi.
-                </p>
-                <p>
-                    Sed sem nisi, luctus consequat ligula in, congue sodales
-                    nisl.
-                </p>
-                <p>
-                    Vestibulum bibendum at erat sit amet pulvinar. Pellentesque
-                    pharetra leo vitae tristique rutrum. Donec ut volutpat ante,
-                    ut suscipit leo.
-                </p>
-                <h2>Sub-header</h2>
-                <p>
-                    Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum
-                    fringilla aliquet. Pellentesque auctor vehicula malesuada.
-                    Aliquam id feugiat sem, sit amet tempor nulla. Quisque
-                    fermentum felis faucibus, vehicula metus ac, interdum nibh.
-                    Curabitur vitae convallis ligula. Integer ac enim vel felis
-                    pharetra laoreet. Interdum et malesuada fames ac ante ipsum
-                    primis in faucibus. Pellentesque hendrerit ac augue quis
-                    pretium.
-                </p>
-                <p>
-                    Morbi ut scelerisque nibh. Integer auctor, massa non dictum
-                    tristique, elit metus efficitur elit, ac pretium sapien nisl
-                    nec ante. In et ex ultricies, mollis mi in, euismod dolor.
-                </p>
-                <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
-                <p>
-                    Bold - React Native, Node.js, PostgresSQL A fitness tracking
-                    mobile app that allows users to scan food product barcodes
-                    to display macronutrients, track body metrics, set notes and
-                    reminders, and get recipe suggestions. The metrics page
-                    displays total macronutrient intake for the day. Social
-                    Network Platform - React/Redux, Node.js, PostgresSQL A
-                    social network platform with features like creating profile
-                    pages, updating bio and profile picture, adding, rejecting
-                    or accepting friend requests, and using a chat feature
-                    through socket.io. The backend allows users to register,
-                    save their information, edit profiles, and reset or change
-                    passwords. Imageboard - Vue.js, Node.js, PostgresSQL, AWS An
-                    anonymous image board where users can upload pictures, add
-                    comments, and like photos. The app is fully responsive and
-                    utilizes AWS bucket for image hosting.
-                </p>
+                <h2 style={{ fontWeight: "bolder" }}>Gallery</h2>
+                <hr />
+                <br />
+                Carousel
+                <br />
             </article>
+            <br />
+
             <motion.div className="progress-bar" style={{ scaleX }} />
         </>
     );
