7908f8fa8fc52d453117583d544cbcaf640d688f
HASSAN <hassan.aburesha@gmail.com>
2023-04-03 12:36:22 +0200

    project3 content done

--- pages/projects/3.tsx
+++ pages/projects/3.tsx
@@ -1,5 +1,11 @@
 import { motion, useScroll, useSpring } from "framer-motion";
 import Link from "next/link";
+import Image from "next/image";
+import { FaGithub } from "react-icons/fa";
+import { Carousel } from "@mantine/carousel";
+import pixla1 from "../../public/pixla_pics/pixla1.png";
+import pixla2 from "../../public/pixla_pics/pixla2.png";
+import pixla3 from "../../public/pixla_pics/pixla3.gif";
 
 function Project3() {
     const { scrollYProgress } = useScroll();
@@ -25,111 +31,140 @@ function Project3() {
                     </a>
                 </div>
             </nav>
+
             <h1>
-                <code>Image Board</code> with spring smoothing
+                <code style={{ fontWeight: "bold", fontSize: "1.5em" }}>
+                    PIXLA{" "}
+                </code>
+                <Link
+                    target={"_blank"}
+                    href="https://github.com/h-aburesha/vue-imageboard-project"
+                >
+                    <FaGithub />
+                </Link>
+                <br />
+                <code
+                    style={{
+                        fontStyle: "italic",
+                        fontWeight: "lighter",
+                        fontSize: "0.8em",
+                    }}
+                >
+                    Vue Image Board
+                </code>
             </h1>
 
             <article>
+                <h2 style={{ fontWeight: "bolder" }}>Description</h2>
+                <hr />
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Aliquam ac rhoncus quam.
+                    A Vue.js single page Images Gallery application. The
+                    Back-end is over an express-Server, PostgresSQL database &
+                    AWS Bucket for images upload.
+                </p>
+                <h2 style={{ fontWeight: "bolder" }}>Features</h2>
+                <hr />
+                <ul>
+                    <li>
+                        Anonymous Image Upload: One of the key features of the
+                        app was the ability for anonymous users to upload
+                        pictures and add titles and descriptions. This feature
+                        was implemented using a simple form with file upload
+                        capabilities, which would allow users to easily add new
+                        images to the gallery without needing to sign up or log
+                        in.
+                    </li>
+                    <li>
+                        Automated Gallery Rendering: To ensure that new images
+                        were immediately displayed in the gallery after being
+                        uploaded, an automatic rendering feature was
+                        implemented. This meant that the gallery would update in
+                        real-time, providing a seamless and convenient user
+                        experience.
+                    </li>
+                    <li>
+                        Image Enlargement and Commenting: Users could click on
+                        any image in the gallery to enlarge it and view
+                        additional information. They could also add comments,
+                        which would be instantly displayed alongside the image.
+                        This feature was designed to encourage user engagement
+                        and facilitate community building.
+                    </li>
+                    <li>
+                        Integrated Like Functionality: Finally, the app
+                        integrated a fully functional like system, which allowed
+                        users to indicate their appreciation for specific
+                        images. This feature was fully integrated with the
+                        backend database, ensuring that likes were accurately
+                        recorded and displayed in real-time.
+                    </li>
+                </ul>
+                <h2 style={{ fontWeight: "bolder" }}>Challenges</h2>
+                <hr />
+                <p>
+                    Syntax Learning Curve: Coming from a non-framework
+                    background, learning the Vue.js syntax was initially quite
+                    challenging. However, as you got more familiar with the
+                    framework, you were able to appreciate the strengths and
+                    benefits of using a JS framework.
                 </p>
                 <p>
-                    Fringilla quam urna. Cras turpis elit, euismod eget ligula
-                    quis, imperdiet sagittis justo. In viverra fermentum ex ac
-                    vestibulum. Aliquam eleifend nunc a luctus porta. Mauris
-                    laoreet augue ut felis blandit, at iaculis odio ultrices.
-                    Nulla facilisi. Vestibulum cursus ipsum tellus, eu tincidunt
-                    neque tincidunt a.
-                </p>
-                <h2>Sub-header</h2>
-                <p>
-                    In eget sodales arcu, consectetur efficitur metus. Duis
-                    efficitur tincidunt odio, sit amet laoreet massa fringilla
-                    eu.
+                    Modal Components and Prop Passing: Implementing modal
+                    components and passing props in Vue.js was not as
+                    straightforward as it was in React, which led to some
+                    difficulty in maintaining visibility and readability of the
+                    code. However, with practice and experience, you were able
+                    to develop better strategies for managing props and
+                    maintaining clear code structure.
                 </p>
                 <p>
-                    Pellentesque id lacus pulvinar elit pulvinar pretium ac non
-                    urna. Mauris id mauris vel arcu commodo venenatis. Aliquam
-                    eu risus arcu. Proin sit amet lacus mollis, semper massa ut,
-                    rutrum mi.
+                    Form Submission and AWS Setup: Setting up form submission
+                    and AWS bucket integration were also part of the learning
+                    curve with JS frameworks. While challenging at first, these
+                    experiences provided valuable opportunities for you to
+                    develop your skills and gain exposure to new technologies
+                    that you could leverage in future projects.
                 </p>
-                <p>
-                    Sed sem nisi, luctus consequat ligula in, congue sodales
-                    nisl.
-                </p>
-                <p>
-                    Vestibulum bibendum at erat sit amet pulvinar. Pellentesque
-                    pharetra leo vitae tristique rutrum. Donec ut volutpat ante,
-                    ut suscipit leo.
-                </p>
-                <h2>Sub-header</h2>
-                <p>
-                    Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum
-                    fringilla aliquet. Pellentesque auctor vehicula malesuada.
-                    Aliquam id feugiat sem, sit amet tempor nulla. Quisque
-                    fermentum felis faucibus, vehicula metus ac, interdum nibh.
-                    Curabitur vitae convallis ligula. Integer ac enim vel felis
-                    pharetra laoreet. Interdum et malesuada fames ac ante ipsum
-                    primis in faucibus. Pellentesque hendrerit ac augue quis
-                    pretium.
-                </p>
-                <p>
-                    Morbi ut scelerisque nibh. Integer auctor, massa non dictum
-                    tristique, elit metus efficitur elit, ac pretium sapien nisl
-                    nec ante. In et ex ultricies, mollis mi in, euismod dolor.
-                </p>
-                <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
-                <p>
-                    Pellentesque id lacus pulvinar elit pulvinar pretium ac non
-                    urna. Mauris id mauris vel arcu commodo venenatis. Aliquam
-                    eu risus arcu. Proin sit amet lacus mollis, semper massa ut,
-                    rutrum mi.
-                </p>
-                <p>
-                    Sed sem nisi, luctus consequat ligula in, congue sodales
-                    nisl.
-                </p>
-                <p>
-                    Vestibulum bibendum at erat sit amet pulvinar. Pellentesque
-                    pharetra leo vitae tristique rutrum. Donec ut volutpat ante,
-                    ut suscipit leo.
-                </p>
-                <h2>Sub-header</h2>
-                <p>
-                    Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum
-                    fringilla aliquet. Pellentesque auctor vehicula malesuada.
-                    Aliquam id feugiat sem, sit amet tempor nulla. Quisque
-                    fermentum felis faucibus, vehicula metus ac, interdum nibh.
-                    Curabitur vitae convallis ligula. Integer ac enim vel felis
-                    pharetra laoreet. Interdum et malesuada fames ac ante ipsum
-                    primis in faucibus. Pellentesque hendrerit ac augue quis
-                    pretium.
-                </p>
-                <p>
-                    Morbi ut scelerisque nibh. Integer auctor, massa non dictum
-                    tristique, elit metus efficitur elit, ac pretium sapien nisl
-                    nec ante. In et ex ultricies, mollis mi in, euismod dolor.
-                </p>
-                <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
-                <p>
-                    Bold - React Native, Node.js, PostgresSQL A fitness tracking
-                    mobile app that allows users to scan food product barcodes
-                    to display macronutrients, track body metrics, set notes and
-                    reminders, and get recipe suggestions. The metrics page
-                    displays total macronutrient intake for the day. Social
-                    Network Platform - React/Redux, Node.js, PostgresSQL A
-                    social network platform with features like creating profile
-                    pages, updating bio and profile picture, adding, rejecting
-                    or accepting friend requests, and using a chat feature
-                    through socket.io. The backend allows users to register,
-                    save their information, edit profiles, and reset or change
-                    passwords. Imageboard - Vue.js, Node.js, PostgresSQL, AWS An
-                    anonymous image board where users can upload pictures, add
-                    comments, and like photos. The app is fully responsive and
-                    utilizes AWS bucket for image hosting.
-                </p>
+                <h2 style={{ fontWeight: "bolder" }}>Gallery</h2>
+                <hr />
+                <br />
+
+                <Carousel
+                    maw="auto"
+                    mx="auto"
+                    withIndicators
+                    height="auto"
+                    controlsOffset="xs"
+                    loop
+                >
+                    <Carousel.Slide>
+                        <Image
+                            src={pixla1}
+                            alt="pixla vue.js image board one page application home gallery and upload feature"
+                            placeholder="blur"
+                        />
+                    </Carousel.Slide>
+                    <Carousel.Slide>
+                        <Image
+                            src={pixla2}
+                            alt="pixla vue.js image board one page application click to enlarge and add comments modal component"
+                            placeholder="blur"
+                        />
+                    </Carousel.Slide>
+                    <Carousel.Slide>
+                        <Image
+                            src={pixla3}
+                            alt="pixla vue.js image board one page application likes synched with database and gif picture possibility"
+                            width={200}
+                            height={320}
+                        />
+                    </Carousel.Slide>
+                </Carousel>
             </article>
+            <br />
+
+            <br />
+            <br />
             <motion.div className="progress-bar" style={{ scaleX }} />
         </>
     );
