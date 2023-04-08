108ec46ca2ecbc689767622c8cc488bc862ae90e
HASSAN <hassan.aburesha@gmail.com>
2023-04-08 09:05:05 +0200

    about content

--- pages/about.tsx
+++ pages/about.tsx
@@ -55,42 +55,31 @@ const AboutMe = () => {
                     alt="my-stack-cover"
                     placeholder="blur"
                 />
-
-                <h2>Brief Introduction</h2>
+                <h2>INTRODUCTION</h2>
                 <hr />
                 <p>
                     I am a junior full-stack developer with a background in
-                    UX/UI design and DevOps account management. I am passionate
-                    about creating user-friendly web and mobile applications,
-                    and I possess a wealth of knowledge in various technologies,
-                    including JavaScript, React.js, React Native, Vue.js 2,
-                    Node.js, TypeScript, and Figma.
+                    UX/UI design and DevOps account management. My passion is
+                    creating user-friendly web and mobile applications. I strong
+                    knowledge in various technologies, including NextJS,
+                    ReactJS, React Native, VueJS, NodeJS, TypeScript, and Figma.
                 </p>
-                <h2>Currently</h2>
+                <h2>CURRENTLY</h2>
                 <hr />
                 <p>
-                    I have completed a web development bootcamp at Spiced
-                    Academy GmbH, where I acquired intensive, project-based
-                    training in JavaScript, HTML, and CSS. The course focused on
-                    the stack of React, React Native, Vue.js, Node.js,
-                    TypeScript, and Node. During the bootcamp, I completed
-                    various projects, including a fitness tracking mobile app
-                    that allows users to scan food product barcodes, a social
-                    network platform, and an anonymous image board.
-                </p>
-
-                <p>
-                    In addition, I have completed a UX/UI design bootcamp at
-                    Ironhack GmbH, where I learned the foundations of design
-                    principles and design thinking, how to build empathy with
-                    users, create user personas and flows, and incorporate UX
-                    writing and storytelling. During the bootcamp, I completed
-                    various UI projects, including a fitness tracking mobile
-                    app, an editorial magazine, and a professional photographer
+                    I completed intensive, project-based training in web
+                    development and UX/UI design at Spiced Academy GmbH and
+                    Ironhack GmbH, respectively. At Spiced, I learned
+                    JavaScript, HTML, CSS, and various technologies such as
+                    React, Vue.js, Node.js, and TypeScript. I developed multiple
+                    projects including a fitness tracking app, social network
+                    platform, and an anonymous image board. During the Ironhack
+                    bootcamp, I focused on design principles, user empathy, and
+                    UX writing, and completed various UI projects including a
+                    fitness tracking app, editorial magazine, and photographer
                     website.
                 </p>
-
-                <h2>Previously</h2>
+                <h2>PREVIOUSLY</h2>
                 <hr />
                 <p>
                     I hold a Masters degree in Economics from
@@ -101,11 +90,11 @@ const AboutMe = () => {
                     of experience in account management, market/financial
                     analysis, and creative customer solutions.
                 </p>
-                <h2>Skill-set</h2>
+                <h2>SKILL-SET</h2>
                 <hr />
                 <p>
-                    I possess excellent skills in customer orientation,
-                    problem-solving, conflict management, market, and financial
+                    I hold excellent skills in customer orientation,
+                    problem-solving, conflict management, market and financial
                     analysis, as well as user experience design. I am a native
                     English and Arabic speaker with full working proficiency in
                     German.
