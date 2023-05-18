e794e012ff0345ac5ac3ab871a2e206e2854a4fe
HASSAN <hassan.aburesha@gmail.com>
2023-05-18 16:08:26 +0200

    removed parrallax

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,143 +1,110 @@
 import { useRef } from "react";
 import { useEffect, useState } from "react";
-import {
-    motion,
-    useScroll,
-    useSpring,
-    useTransform,
-    MotionValue,
-} from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 
 function ProjectImage({ id }: { id: number }) {
-    const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-    function useParallax(value: MotionValue<number>, distance: number) {
-        useEffect(() => {
-            setIsMobile(window.innerWidth < 768);
-        }, []);
+  useEffect(() => {
+    setIsMobile(window.innerWidth < 768);
+  }, []);
 
-        const newDistance = isMobile ? distance / 2 : distance;
-        return useTransform(value, [0, 1], [-newDistance, newDistance]);
-    }
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
 
-    const ref = useRef(null);
-    const { scrollYProgress } = useScroll({ target: ref });
-    const y = useParallax(scrollYProgress, 300);
+  const projectTitles = [
+    {
+      title: "Bold",
+      description: "React Native - Mobile Application",
+    },
+    {
+      title: "Jam",
+      description: "React - Social Network Platform",
+    },
+    {
+      title: "Pixla",
+      description: "Vue - Image Board",
+    },
+    {
+      title: "Till Van Loosen",
+      description: "UX Study & Figma UI",
+    },
+    {
+      title: "Bold Fitness Tracker",
+      description: "UX Study & Figma UI",
+    },
+  ];
 
-    const projectTitles = [
-        {
-            title: "Bold",
-            description: "React Native - Mobile Application",
-        },
-        {
-            title: "Jam",
-            description: "React - Social Network Platform",
-        },
-        {
-            title: "Pixla",
-            description: "Vue - Image Board",
-        },
-        {
-            title: "Till Van Loosen",
-            description: "UX Study & Figma UI",
-        },
-        {
-            title: "Bold Fitness Tracker",
-            description: "UX Study & Figma UI",
-        },
-    ];
+  return (
+    <>
+       
+      <section>
+        <div ref={ref}>
+          <Link href={`/projects/${id}`}>
+            <Image
+              className="projects-image"
+              src={`/${id}.jpg`}
+              alt={projectTitles[id - 1].description}
+              width={isMobile ? undefined : 1000}
+              height={isMobile ? undefined : 500}
+            />
+          </Link>
+        </div>
+        <div className="projects-title-div">
+          <motion.h2>{projectTitles[id - 1].title}</motion.h2>
+          <motion.p
+            style={{
+              fontWeight: "lighter",
+              fontStyle: "normal",
+              marginTop: "10px",
+            }}
+          >
+            {projectTitles[id - 1].description}
 
-    return (
-        <>
-            <nav>
-                <div
-                    style={{
-                        position: "fixed",
-                        justifyContent: "flex-start",
-                    }}
-                    className="navigation-bar"
-                >
-                    <Link href="/">
-                        <button
-                            style={{
-                                fontSize: "1em",
-                            }}
-                        >
-                            &#8629;
-                        </button>
-                    </Link>
-                </div>
-            </nav>
-            <section>
-                <div ref={ref}>
-                    <Link href={`/projects/${id}`}>
-                        <Image
-                            className="projects-image"
-                            src={`/${id}.jpg`}
-                            alt={projectTitles[id - 1].description}
-                            width={isMobile ? undefined : 1000}
-                            height={isMobile ? undefined : 500}
-                        />
-                    </Link>
-                </div>
-                <div className="projects-title-div">
-                    <motion.h2 style={{ y }}>
-                        {projectTitles[id - 1].title}
-                    </motion.h2>
-                    <motion.p
-                        style={{
-                            y: y,
-                            fontWeight: "lighter",
-                            fontStyle: "normal",
-                            marginTop: "10px",
-                        }}
-                    >
-                        {projectTitles[id - 1].description}
-
-                        <Link
-                            style={{
-                                fontWeight: "lighter",
-                                fontStyle: "normal",
-                                textDecoration: "none",
-                                fontSize: "0.8em",
-                                fontFamily: "Arial, sans-serif",
-                                color: "white",
-                                transition: "color 0.3s ease",
-                            }}
-                            href={`/projects/${id}`}
-                            onMouseOver={(e) =>
-                                (e.currentTarget.style.color = "#646cff")
-                            }
-                            onMouseOut={(e) =>
-                                (e.currentTarget.style.color = "white")
-                            }
-                        >
-                            <br />
-                            More details ...
-                        </Link>
-                    </motion.p>
-                </div>
-            </section>
-        </>
-    );
+            <Link
+              style={{
+                fontWeight: "lighter",
+                fontStyle: "normal",
+                textDecoration: "none",
+                fontSize: "0.8em",
+                fontFamily: "Arial, sans-serif",
+                color: "white",
+                transition: "color 0.3s ease",
+              }}
+              href={`/projects/${id}`}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.color = "#646cff")
+              }
+              onMouseOut={(e) =>
+                (e.currentTarget.style.color = "white")
+              }
+            >
+              <br />
+              More details ...
+            </Link>
+          </motion.p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default function Projects() {
-    const { scrollYProgress } = useScroll();
-    const scaleX = useSpring(scrollYProgress, {
-        stiffness: 100,
-        damping: 30,
-        restDelta: 0.001,
-    });
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
-    return (
-        <>
-            {[1, 2, 3, 4, 5].map((image) => (
-                <ProjectImage key={image} id={image} />
-            ))}
-            <motion.div className="progress" style={{ scaleX }} />
-        </>
-    );
+  return (
+    <>
+      {[1, 2, 3, 4, 5].map((image) => (
+        <ProjectImage key={image} id={image} />
+      ))}
+      <motion.div className="progress" style={{ scaleX }} />
+    </>
+  );
 }
