eb71d9695071c0720cc53902ca8003b0867b309a
HASSAN <hassan.aburesha@gmail.com>
2023-11-03 16:06:50 +0100

    animations for landing container

---
+++ pages/components/landing-container/index.tsx
@@ -0,0 +1,20 @@
+import { motion } from 'framer-motion';
+
+
+const LandingContainer = () => {
+  return (
+    <motion.div className="landing-container" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+      <header className="header">
+      <motion.code className="welcome-message" initial={{ y: 30, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.5, duration: 1 }}>
+          FULL-STACK WEB DEVELOPER & UX/UI DESIGNER.
+          <hr />
+        </motion.code>
+        <motion.code initial={{ x: -100, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 2, duration: 1 }}>
+          For social accounts & resume, please visit footer.
+        </motion.code>
+      </header>
+    </motion.div>
+  );
+};
+
+export default LandingContainer;
