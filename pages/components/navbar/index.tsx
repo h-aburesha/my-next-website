0e5678a4f7645ebd0265e7e0fb599465995454e6
HASSAN <hassan.aburesha@gmail.com>
2023-12-01 18:59:22 +0100

    Fix navbar and add padding to hr element

--- pages/components/navbar/index.tsx
+++ pages/components/navbar/index.tsx
@@ -19,7 +19,6 @@ function Navbar() {
             <Link href="/about">
                 <button>ABOUT</button>
             </Link>
-
             <a href="mailto:hassan.aburesha@gmail.com" target="_blank">
                 <button>EMAIL</button>
             </a>
