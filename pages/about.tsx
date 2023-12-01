fb8b51463178a9c9a6c23789b87b65e4781c5ab3
HASSAN <hassan.aburesha@gmail.com>
2023-12-01 15:39:53 +0100

    Add Navbar component

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,25 +4,12 @@ import Image from "next/image";
 import me from "../public/me.png";
 import myStackCover from "../public/my-stack-cover.png";
 import Link from "next/link";
+import Navbar from "./components/navbar";
 
 const AboutMe = () => {
     return (
         <>
-            <header>
-                <div className="navigation-bar">
-                    <Link href="/">
-                        <button>HOME</button>
-                    </Link>
-                    <Link href="/projects">
-                        <button>PROJECTS</button>
-                    </Link>
-
-                    <a href="mailto:hassan.aburesha@gmail.com" target="_blank">
-                        <button>EMAIL</button>
-                    </a>
-                </div>
-            </header>
-
+            <Navbar />
             <div className="profile-picture">
                 <Image
                     alt="hassan-aburesha"
