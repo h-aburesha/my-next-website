73dbb8ee1979abd5d264726c244ad04d694e0925
HASSAN <hassan.aburesha@gmail.com>
2023-12-03 20:01:23 +0100

    Refactor landing-container component and add prettierrc.json

--- pages/components/landing-container/index.tsx
+++ pages/components/landing-container/index.tsx
@@ -1,20 +1,34 @@
-import { motion } from 'framer-motion';
-
+import { motion } from "framer-motion";
 
 const LandingContainer = () => {
-  return (
-    <motion.div className="landing-container" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
-      <header className="header">
-      <motion.code className="welcome-message" initial={{ y: 30, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.5, duration: 1 }} style={{ textTransform: 'uppercase' }}>
-          FRONT-END SOFTWARE ENGINEER & UX/UI DESIGNER.
-          <hr />
-        </motion.code>
-        <motion.code initial={{ x: -100, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 2, duration: 1 }}>
-          For Github profile, please visit footer.
-        </motion.code>
-      </header>
-    </motion.div>
-  );
+    return (
+        <motion.div
+            className="landing-container"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+        >
+            <header className="header">
+                <motion.code
+                    className="welcome-message"
+                    initial={{ y: 30, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ delay: 0.5, duration: 1 }}
+                    style={{ textTransform: "uppercase" }}
+                >
+                    FRONT-END SOFTWARE ENGINEER & UX/UI DESIGNER.
+                    <hr />
+                </motion.code>
+                <motion.code
+                    initial={{ x: -100, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ delay: 2, duration: 1 }}
+                >
+                    For Github profile, please visit footer.
+                </motion.code>
+            </header>
+        </motion.div>
+    );
 };
 
 export default LandingContainer;
