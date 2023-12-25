5d8cca9e08b8c2479827e86a3e2320c3f70f169d
HASSAN <hassan.aburesha@gmail.com>
2023-12-25 18:04:10 +0100

    Update meta tags in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,14 +27,26 @@ export default function Document() {
                     content="Dn8khY8NOOtwdCmKNweUxEqzngzd5GYmEbMOJd9fh_0"
                 />
                 <meta property="og:title" content="Hassan Aburesha" />
-                <meta property="og:description" content="Hassan Aburesha's Portfolio" />
-                <meta property="og:image" content={`${process.env.NEXT_PUBLIC_BASE_URL}/website-small.png`} />
+                <meta
+                    property="og:description"
+                    content="Hassan Aburesha's Portfolio"
+                />
+                <meta
+                    property="og:image"
+                    content={`${process.env.NEXT_PUBLIC_BASE_URL}/website-small.png`}
+                />
                 <meta property="og:url" content="https://www.aburesha.com/" />
                 <meta property="og:type" content="website" />
-                <meta property="og:site_name" content="Hassan Aburesha's Portfolio" />
+                <meta
+                    property="og:site_name"
+                    content="Hassan Aburesha's Portfolio"
+                />
                 <meta name="robots" content="index, follow" />
                 <meta name="author" content="Hassan Aburesha" />
-                <meta name="keywords" content="Developer,UX,UI,Berlin,ux designer" />
+                <meta
+                    name="keywords"
+                    content="Developer,UX,UI,Berlin,Designer"
+                />
                 <link rel="canonical" href="https://www.aburesha.com/" />
             </Head>
             <body>
