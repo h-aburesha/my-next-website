e33f6b344446fb89b14098c72d8e4ea3f8df2844
HASSAN <hassan.aburesha@gmail.com>
2024-01-18 14:01:08 +0100

    Fix typo in metrics page

--- pages/projects/1.tsx
+++ pages/projects/1.tsx
@@ -62,7 +62,7 @@ function Project1() {
                     <li>Tracking of body metrics</li>
                     <li>Capability for personal notes and reminders.</li>
                     <li>Recipe suggestions</li>
-                    <li>Metrics page displaying total macronutrient intake.</li>
+                    <li>Metrics page displaying total macronutrient intake</li>
                     <li>
                         Personal profile page with BMI, photo, and dark/light
                         mode toggle
