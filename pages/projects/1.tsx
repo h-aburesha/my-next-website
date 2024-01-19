e5463970c72c7870da7ddcf4ed7b08519e8e4ca2
HASSAN <hassan.aburesha@gmail.com>
2024-01-19 16:28:44 +0100

    Refactor metrics page to display total macronutrients intake

--- pages/projects/1.tsx
+++ pages/projects/1.tsx
@@ -62,7 +62,9 @@ function Project1() {
                     <li>Tracking of body metrics.</li>
                     <li>Capability for personal notes and reminders.</li>
                     <li>Recipe suggestions.</li>
-                    <li>Metrics page displaying total macronutrients intake</li>
+                    <li>
+                        Metrics page displaying total macronutrients intake.
+                    </li>
                     <li>
                         Personal profile page with BMI, photo, and dark/light
                         mode toggle
