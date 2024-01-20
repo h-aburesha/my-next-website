67a9990316eb988b58ce449e26b08d79078403bc
HASSAN <hassan.aburesha@gmail.com>
2024-01-20 13:40:10 +0100

    Fix formatting in project 1 page

--- pages/projects/1.tsx
+++ pages/projects/1.tsx
@@ -61,7 +61,7 @@ function Project1() {
                     </li>
                     <li>Tracking of body metrics</li>
                     <li>Capability for personal notes and reminders</li>
-                    <li>Recipe suggestions.</li>
+                    <li>Recipe suggestions</li>
                     <li>
                         Metrics page displaying total macronutrients intake.
                     </li>
