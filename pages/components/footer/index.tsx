f1ff94304e7ac28348ec3426b8899073b190e260
HASSAN <hassan.aburesha@gmail.com>
2024-06-30 20:02:21 +0200

    Added docs

--- pages/components/footer/index.tsx
+++ pages/components/footer/index.tsx
@@ -1,16 +1,24 @@
-import { FaLinkedin, FaGithub, FaDownload } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaDownload } from 'react-icons/fa'
 
-function Footer({ title = "All rights reserved" }) {
+/**
+ * Renders the footer component.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.title - The title to display in the footer. Defaults to "All rights reserved".
+ * @returns {JSX.Element} The rendered footer component.
+ */
+
+function Footer({ title = 'All rights reserved' }) {
     return (
         <footer>
             <div className="footer-icons">
                 <a
-                    target={"_blank"}
+                    target={'_blank'}
                     href="https://www.linkedin.com/in/haburesha/"
                 >
                     <FaLinkedin />
                 </a>
-                <a target={"_blank"} href="https://github.com/h-aburesha">
+                <a target={'_blank'} href="https://github.com/h-aburesha">
                     <FaGithub />
                 </a>
                 <a href="/hassan_aburesha_cv.pdf" download>
@@ -19,7 +27,7 @@ function Footer({ title = "All rights reserved" }) {
             </div>
             <code>{title} &copy;</code>
         </footer>
-    );
+    )
 }
 
-export default Footer;
+export default Footer
