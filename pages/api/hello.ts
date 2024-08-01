d6477fff68697fbd786e8e35be779a9dee1152b5
Hassan <hassan.aburesha@justwatch.com>
2024-08-01 12:28:02 +0200

    Refactor hello.ts API handler to improve code formatting and add JSDoc comments

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -7,7 +7,7 @@ type Data = {
 
 /**
  * Handles the API request and sends a JSON response.
- * @param req - The NextApiRequest object representing the incoming request.
+ * @param req - The NextApiRequest object representing the incoming request
  * @param res - The NextApiResponse object representing the outgoing response
  */
 export default function handler(
